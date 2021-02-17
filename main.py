85b58b1b0c4b18bc9c3e64939591b521d98392bd
Danielle Jenkins <jenkinda@uw.edu>
2021-02-17 04:38:15 -0800

    Add async client library for interacting with digital ocean api.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sanic import Sanic
 from sanic.response import json
 from sanic.log import logger
+import httpx
 
 app = Sanic("NaaS")
 
@@ -13,12 +14,27 @@ async def instance_endpoint(request, instance_id):
 
 @app.route("/neos/instance/<instance_id>/create", methods=['POST'])
 async def instance_endpoint(request, instance_id):
+    async with httpx.AsyncClient() as client:
+        r = await client.post("https://api.digitalocean.com/v2/droplets", headers={"Content-Type": "application/json",
+                                                                                   "Authorization": "Bearer b7d03a6947b217efb6f3ec3bd3504582"},
+                              data={"name": "instance_name",
+                                    "region": "nyc3",
+                                    "size": "s-1vcpu-1gb",
+                                    "image": "ubuntu-16-04-x64",
+                                    "ssh_keys": [107149],
+                                    "backups": False,
+                                    "ipv6": True,
+                                    "user_data": None,
+                                    "private_networking": None,
+                                    "volumes": None,
+                                    "tags": ["web"]})
     return json({"status": "created", "instance_id": instance_id})
 
 
 @app.route("/neos/instance/<instance_id>/delete", methods=['POST'])
 async def instance_endpoint(request, instance_id):
     return json({"status": "deleted", "instance_id": instance_id})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True, access_log=True)
