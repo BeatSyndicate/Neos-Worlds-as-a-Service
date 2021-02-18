13f0a2288b37b5ab6cf05a89a73d3699b53d7b74
Danielle Jenkins <jenkinda@uw.edu>
2021-02-17 23:15:48 -0800

    Only listen on localhost for security.

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ async def instance_delete_endpoint(request, instance_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True, access_log=True, workers=os.cpu_count())
+    app.run(host="127.0.0.1", port=8000, debug=True, access_log=True, workers=os.cpu_count())
