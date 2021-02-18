1823e4063963a4eba35c6f07c95e4e6aadceffb1
Danielle Jenkins <jenkinda@uw.edu>
2021-02-17 23:15:30 -0800

    Actually auth and create basic digitalocean server and return response.

--- main.py
+++ main.py
@@ -13,52 +13,60 @@ DIGITALOCEAN_COMMON_HEADERS = {
 
 
 @app.route("/neos/instance/<instance_id>", methods=['GET'])
-async def instance_endpoint(request, instance_id):
+async def instance_get_endpoint(request, instance_id):
     # logger.info("route='/neos/instance/%s", instance_id)
     async with httpx.AsyncClient() as client:
         r = await client.post(
             "https://api.digitalocean.com/v2/droplets?tag_name={instance_id}".format(instance_id=instance_id),
             headers=DIGITALOCEAN_COMMON_HEADERS,
         )
-    # TODO: parse and return vm info
-    return json({"status": "healthy", "instance_id": instance_id, "log_websocket": ""})
+    if r.is_error:
+        logger.info("request={}".format(r.request.__dict__))
+        logger.error("response={}".format(r.json()))
+        return json({"status": "error", "message": r.reason_phrase})
+    else:
+        return json({"status": "healthy", "instance_id": instance_id, "log_websocket": ""})
 
 
 # Unfortunately Neos lacks a DELETE HTTP request logix node so we have to put the verb in the method.
 @app.route("/neos/instance/<instance_id>/create", methods=['POST'])
-async def instance_endpoint(request, instance_id):
+async def instance_create_endpoint(request, instance_id):
     async with httpx.AsyncClient() as client:
         r = await client.post(
             "https://api.digitalocean.com/v2/droplets",
             headers=DIGITALOCEAN_COMMON_HEADERS,
-            data={
+            json={
                 "name": "neos-server-{}".format(instance_id),
                 "region": "sfo3",
                 "size": "s-1vcpu-1gb",
                 "image": "ubuntu-16-04-x64",
-                "ssh_keys": [107149],
-                "backups": False,
                 "ipv6": True,
-                "user_data": None,
-                "private_networking": None,
-                "volumes": None,
+                "monitoring": True,
                 "tags": ["neos", instance_id]
             }
         )
-    # TODO: actually check status code
-    return json({"status": "created", "instance_id": instance_id})
+        if r.is_error:
+            logger.info("request={}".format(r.request.__dict__))
+            logger.error("response={}".format(r.json()))
+            return json({"status": "error", "message": r.reason_phrase})
+        else:
+            return json({"status": "created", "instance_id": instance_id})
 
 
 # Unfortunately Neos lacks a DELETE HTTP request logix node so we have to put the verb in the method.
 @app.route("/neos/instance/<instance_id>/delete", methods=['POST'])
-async def instance_endpoint(request, instance_id):
+async def instance_delete_endpoint(request, instance_id):
     async with httpx.AsyncClient() as client:
         r = await client.post(
             "https://api.digitalocean.com/v2/droplets?tag_name={instance_id}".format(instance_id=instance_id),
             headers=DIGITALOCEAN_COMMON_HEADERS
         )
-    # TODO: actually check status code
-    return json({"status": "deleted", "instance_id": instance_id})
+    if r.is_error:
+        logger.info("request={}".format(r.request.__dict__))
+        logger.error("response={}".format(r.json()))
+        return json({"status": "error", "message": r.reason_phrase})
+    else:
+        return json({"status": "deleted", "instance_id": instance_id})
 
 
 if __name__ == "__main__":
