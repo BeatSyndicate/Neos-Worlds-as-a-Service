5b1c5c60ef2da0cdead161c424c2bed16a3b30d7
Danielle Jenkins <jenkinda@uw.edu>
2021-03-11 02:31:28 -0800

    Remove sudo and adjust for debpkgs prompts.

--- base_image/provision.sh
+++ base_image/provision.sh
@@ -8,30 +8,33 @@ set -xe
 sleep 30
 
 # Install base utils
-sudo apt-get update && sudo apt-get upgrade -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew
-sudo apt-get install -y systemd mosh htop tmux
-sudo systemctl restart systemd-journald # otherwise journald isn't recording
+apt-get update
+apt-get upgrade -y -o Dpkg::Options::=--force-confnew
+apt-get install -y systemd mosh htop tmux
+systemctl restart systemd-journald # otherwise journald isn't recording
 
 # Configure firewall
-sudo ufw allow 22
-sudo ufw enable
+ufw allow 22
+ufw enable
 
 # Install mono
-sudo apt-get install -y dirmngr gnupg apt-transport-https ca-certificates software-properties-common
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-sudo apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu stable-focal main'
-sudo apt-get update
-sudo apt-get install -y mono-runtime mono-complete
+apt-get install -y dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu stable-focal main'
+apt-get update
+apt-get install -y mono-runtime mono-complete
 
 # Install steamcmd
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository multiverse
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install -y unzip binutils jq netcat lib32stdc++6 libsdl2-2.0-0:i386
+apt-get install -y software-properties-common
+add-apt-repository multiverse
+dpkg --add-architecture i386
+apt-get update
+apt-get install -y unzip binutils jq netcat lib32stdc++6 libsdl2-2.0-0:i386
 echo steam steam/question select "I AGREE" | sudo debconf-set-selections
 echo steam steam/license note '' | sudo debconf-set-selections
-sudo apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install -y lib32gcc1 steamcmd
+apt-get -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confnew install -y lib32gcc1 steamcmd
 
 # Setup steam user
-sudo adduser --disabled-password --shell /bin/bash --gecos 'steam' steam
+adduser --disabled-password --shell /bin/bash --gecos 'steam' steam
+cp -r /root/.ssh /home/steam/
+chown -R steam:steam /home/steam/.ssh
