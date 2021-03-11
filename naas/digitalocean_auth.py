a40dbbfe20b6ac863b582cc58518e147fa7a53c8
Danielle Jenkins <jenkinda@uw.edu>
2021-03-10 17:38:59 -0800

    Change DO key.
    Change the digital ocean key to be consistent
    with packer.

--- naas/digitalocean_auth.py
+++ naas/digitalocean_auth.py
@@ -2,5 +2,5 @@ import os
 
 DIGITALOCEAN_COMMON_HEADERS = {
     "Content-Type": "application/json",
-    "Authorization": "Bearer {token}".format(token=os.environ['DO_API_TOKEN'])
+    "Authorization": "Bearer {token}".format(token=os.environ['DIGITALOCEAN_API_TOKEN'])
 }
