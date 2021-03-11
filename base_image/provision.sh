ff9775f42735b4686c41f4a00eda4522d23f17ea
Danielle Jenkins <jenkinda@uw.edu>
2021-03-10 21:57:18 -0800

    Add firewall to base image.

--- base_image/provision.sh
+++ base_image/provision.sh
@@ -12,6 +12,10 @@ sudo apt-get update && sudo apt-get upgrade -y -o Dpkg::Options::=--force-confde
 sudo apt-get install -y systemd mosh htop tmux
 sudo systemctl restart systemd-journald # otherwise journald isn't recording
 
+# Configure firewall
+sudo ufw allow 22
+sudo ufw enable
+
 # Install mono
 sudo apt-get install -y dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
